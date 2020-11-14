0847dbe12fa81ad7946115186789d6bc686560ca
jrimyak <imyakjake@gmail.com>
2020-11-14 01:25:00 -0500

    routing and home page

--- App.js
+++ App.js
@@ -24,69 +24,26 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-import {createSwitchNavigator, createAppContainer, SwitchNavigator, NavigationEvents} from "react-navigation";
-import { createStackNavigator } from 'react-navigation-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './Components/Home';
+import Login from './Components/Login';
 import Signup from './Components/Signup';
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: Signup
-  }
-})
-
-// const App: () => React$Node = () => {
-//   return (
-   
-//     <AppNavigator />
-//   );
-// };
+const Stack = createStackNavigator();
 
 class App extends Component {
   render() {
     return (
-      <AppNavigator />
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-App = createAppContainer(AppNavigator);
 export default App;
---
+++ Components/Home.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import {Dimensions, Linking, ActivityIndicator, StyleSheet, Text, View, TextInput, TouchableWithoutFeedback, Keyboard, Button, Image, Alert, TouchableOpacity, KeyboardAvoidingView, Picker, AsyncStorage} from 'react-native';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            photoVisible: false
+        }
+    }
+
+    render() {
+        return(
+            <View style = {{flex: 1, backgroundColor: '#9ff4c4'}}>
+                <View style={styles.container}>
+                    <View
+                        style={styles.logoContainer}>
+                        {
+                            this.state.photoVisible && <Image source="" alt="" style={styles.logo}></Image>                        }
+                    </View>
+                    <Text style={{textAlign: "center"}}>Home</Text>
+                    <Text />
+                    <View style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                        <TouchableOpacity 
+                        onPress =  {() => this.props.navigation.navigate("Signup")}
+                        style={styles.authButton}
+                        >
+                            <Text style={styles.loginButtonText}>Log-in</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Text />
+                    <View style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                        <TouchableOpacity 
+                        onPress = {() => this.props.navigation.navigate("Signup")}
+                        style={styles.authButton}>
+                            <Text style={styles.loginButtonText}>Create an Account</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: windowHeight/2
+      },
+    logoContainer: {
+        marginTop: 50,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        //zIndex: -1
+    },
+    loginButtonText: {
+        color: "#A8A8A8",
+        textAlign: "center",
+        fontWeight: "500",
+        fontSize: 16,
+        padding: 5
+    },
+    authButton: {
+        marginTop: 10,
+        padding: 5,
+        borderRadius: 5,
+        color: "#A8A8A8",
+        backgroundColor: "#1E1E1E",
+        width: 150
+    },
+    textfield: {
+        display: "flex",
+        borderWidth: 1,
+        borderColor: "#5891E5",
+        padding: 10, 
+        borderRadius: 50,
+        color: "#000",
+        backgroundColor: "#fff",
+        width: 350
+    }
+});
+
+
+export default Home 
